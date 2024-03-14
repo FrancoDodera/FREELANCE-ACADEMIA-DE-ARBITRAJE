02b0edbb74df12e6fc210a9f264d152b4be284f5
FrancoDodera <francocarreno99@gmail.com>
2024-03-14 15:22:26 -0300

    proyecto

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,66 @@
-import Image from "next/image";
+"use client";
 import styles from "./page.module.css";
+import { useEffect } from "react";
 
 export default function Home() {
+  //Calendly
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = "https://assets.calendly.com/assets/external/widget.js";
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <main className={styles.main}>
+        <div className={styles.container}>
+          <h1 className={styles.h1}>
+            GANÁ TUS PRIMEROS <span className={styles.highlight}> 1.000</span>{" "}
+            USD CON <span className={styles.highlight}> ARBITRAJE</span>
+          </h1>
+          <h2 className={styles.h2}>
+            Ni Marketing, ni Amazon FBA, ni Dropshipping, ni Appointment
+            Setting. Arbitraje Financiero: la{" "}
+            <span className={styles.highlight}> NUEVA HABILIDAD</span> de la que
+            nadie está hablando que te va a permitir generar TUS PROPIOS $1000
+            USD este 2024
+          </h2>
+
+          <h2 className={styles.text}>
+            SI QUERES darle un giro de 180 grados a tu{" "}
+            <span className={styles.highlight}>economia</span> y lograr
+            finalmente tener un ingreso en DOLARES, esto es para vos. Te quiero
+            compartir desde lo mas básico hasta las estrategias mas avanzadas
+            que yo mismo utilicé para pasar de $x a $x en x meses y que te van a
+            PERMITIR a vos tener los mismos resultados operando con total
+            seguridad y sin{" "}
+            <span className={styles.highlight}>NINGUN TIPO DE RIESGO</span> en
+            el mondo del arbitraje cripto
+          </h2>
+          <h2 className={styles.h2}>
+            <span className={styles.highlight}>
+              Agregrar gratiuitamente una asesoria
+            </span>
+          </h2>
+          <div>
+            <div
+              className="calendly-inline-widget"
+              data-url="https://calendly.com/francocarre/asesoria-gratuita-arbitraje-financiero?month=2024-03"
+              style={{ width: "1000px", height: "700px" }}
+            ></div>
+          </div>
+          <h3>wpp</h3>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <footer className={styles.footer}>
+          <h4>©2024 | ArbaAcademy</h4>
+        </footer>
+      </main>
+    </>
   );
 }
